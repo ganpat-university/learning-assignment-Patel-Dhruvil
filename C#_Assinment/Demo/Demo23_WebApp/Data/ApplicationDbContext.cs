fc5621f017b1f7b5df171ee373f598bdf72a803e
Patel-Dhruvil <dhruvil.patel@einfochips.com>
2022-02-11 01:01:44 +0530

    create a department model&view

---
+++ C#_Assinment/Demo/Demo23_WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Demo23_WebApp.Models;
+
+namespace Demo23_WebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ C#_Assinment/Demo/Demo23_WebApp/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Demo23_WebApp.Models
+{
+    [Table("Departments")]
+    public class Department
+    {
+        [Key]
+        [Display(Name ="DepartmentId")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DepartmentId { get; set; }
+
+        [Display(Name ="DepartmentName")]
+        [Required]
+        [StringLength(50,ErrorMessage ="limit of characters is 50")]
+        public string DepartmentName { get; set; }
+    }
+}
