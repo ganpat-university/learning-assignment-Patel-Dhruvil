7b660e83fafb70bdb62518a79e3413b31ef54d1f
Patel-Dhruvil <dhruvil.patel@einfochips.com>
2022-02-14 23:17:47 +0530

    pending upload

---
+++ C#_Assinment/Demo/Demo26_Task/Demo1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Demo26_Task
+{
+    class Demo1
+    {
+        public static void Run()
+        {
+            ThreadStart t
+                = new ThreadStart(()
+                =>
+                {
+                    Console.WriteLine("hello from Thread: {0}", Thread.CurrentThread.ManagedThreadId);
+                });
+
+        }
+    }
+}
