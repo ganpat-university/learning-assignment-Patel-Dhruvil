be574a5de2e4a810520e17cad3215dbf15e54f26
Patel-Dhruvil <dhruvil.patel@einfochips.com>
2022-02-20 16:32:49 +0530

    Angular Demo

---
+++ AngularDemo/MyDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyDemo.Models;
+
+namespace MyDemo.Data
+{
+    public class ApplicationDbContext
+        :DbContext
+    {
+
+        public DbSet<Department> Departments { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
