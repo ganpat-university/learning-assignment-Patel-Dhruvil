f1cb2ec10b8690bc69cab0dc699e07ca9437e3cd
Patel-Dhruvil <dhruvil.patel@einfochips.com>
2022-03-15 21:54:28 +0530

    ReactDemo

---
+++ ReactDemo/ReactProj01/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReactProj01.Models;
+
+namespace ReactProj01.Data
+{
+    public class ApplicationDbContext
+       : DbContext
+    {
+
+        public DbSet<Department> Departments { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
